ff964414571146e85b9bcf044a5cf7eb8747128c
gabrielpacificoo <gabriel.pacifico@sptech.school>
2025-07-10 00:27:23 -0300

    add - api springboot backend

---
+++ mechanic-cloud-spring/src/main/java/mechanic/cloud/MechanicCloudApplication.java
@@ -0,0 +1,13 @@
+package mechanic.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicCloudApplication.class, args);
+	}
+
+}
